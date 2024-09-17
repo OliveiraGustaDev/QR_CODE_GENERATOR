228d598653945e3965d3027ee6cfb8cd585b9260
OliveiraGustaDev <gugibsonj200@gmail.com>
2024-09-17 00:49:14 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,62 +2,59 @@
 
 const container = document.querySelector(".container");
 const qrHeaderTitle = document.querySelector("#qr-header p");
-
 const qrInput = document.querySelector("#qr-form input");
 const generatorButton = document.querySelector("#qr-form button");
 const cleanButton = document.querySelector("#clean");
 const imgQrCode = document.querySelector("#qr-code img");
 const descriptionImg = document.querySelector("#description-img");
+const footer = document.querySelector(".to-hide");
 
-// Funcões
+// Funções
 
 const generateCode = () => {
-  const textQrCodeInput = qrInput.value;
-  if (!textQrCodeInput) {
+  const textQrInput = qrInput.value;
+
+  imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${textQrInput}`;
+  generatorButton.innerHTML = "Carregando...";
+
+  if (!textQrInput) {
     Swal.fire({
       icon: "info",
       title: "Preenchimento Obrigatório",
       text: "É necessário preencher algum texto para prosseguir!",
     });
+
     return;
   }
 
-  imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${textQrCodeInput}`;
-  generatorButton.innerHTML = "Carregando!";
-
   imgQrCode.addEventListener("load", () => {
     container.classList.add("active");
-    generatorButton.innerHTML = "Criado com sucesso!";
-    qrHeaderTitle.innerHTML = "Seu código foi criado e está pronto para uso.";
+    generatorButton.innerHTML = "Criado com Sucesso!";
+    descriptionImg.innerHTML = textQrInput;
+    qrHeaderTitle.innerHTML = "Seu código foi gerado e está pronto para uso.";
     qrHeaderTitle.style.color = "#ffb108";
     qrHeaderTitle.style.fontWeight = "bold";
-
-    descriptionImg.innerHTML = textQrCodeInput;
+    footer.classList.remove("to-hide");
+    footer.classList.add("show");
   });
 };
 
 const restartAll = () => {
   container.classList.remove("active");
+  generatorButton.innerHTML = "Gerar Qr Code";
+  descriptionImg.innerHTML = "";
   qrHeaderTitle.innerHTML = "Insira uma URL ou texto, para criar um QR Code";
+  qrInput.value = "";
   qrHeaderTitle.style.color = "";
   qrHeaderTitle.style.fontWeight = "";
-  generatorButton.innerHTML = "Gerar QR Code";
-  qrInput.value = "";
-  descriptionImg.innerHTML = "";
+  footer.classList.remove("show");
+  footer.classList.add("to-hide");
 };
 
 // Eventos
 
 generatorButton.addEventListener("click", () => {
-  const textQrInput = qrInput.value;
-
   generateCode();
-});
-
-qrInput.addEventListener("keydown", (e) => {
-  if (e.code === "Enter") {
-    generateCode();
-  }
 });
 
 cleanButton.addEventListener("click", () => {
@@ -70,3 +67,8 @@ qrInput.addEventListener("keyup", () => {
   }
 });
 
+qrInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    generateCode();
+  }
+});
